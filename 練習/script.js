b6a5bfa5c2998a441a42cbcd357791e38e2fee05
Xiaoliu0405 <xiaoliu0405@gmail.com>
2025-02-19 08:47:10 +0800

    Add files via upload

---
+++ 練習/script.js
@@ -0,0 +1,10 @@
+function togglevisibility() {
+    let element = document.getElementById("login");
+    if (element.style.visibility === "hidden") {
+        element.style.visibility = "visible";
+    } else {
+        element.style.visibility = "hidden";
+    }
+}
+
+
